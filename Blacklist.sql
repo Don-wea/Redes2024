dba81b9e44c5b31d2867da69c629a5e26eef2ff1
MaximoBarahona <alejandro2003.esco@gmail.com>
2024-11-11 22:46:49 -0300

    Update Blacklist.sql

--- Blacklist.sql
+++ Blacklist.sql
@@ -2,3 +2,10 @@
 CREATE TABLE Blacklist (
     username VARCHAR(25) PRIMARY KEY, reason TEXT
 )
+
+CREATE FUNCTION log_event(p_username VARCHAR,p_event TEXT )
+    RETURNS void as $$
+BEGIN
+    RAISE notice "User: ,Event: ",p_username, p_event;
+END;
+$$ LANGUAGE plpgsql;
